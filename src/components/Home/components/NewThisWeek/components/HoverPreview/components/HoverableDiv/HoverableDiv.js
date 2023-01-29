b708076d1a739774a63e64c6d45fe2b826ba0ba2
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2023-01-29 22:25:25 +0800

    media query

--- src/components/Home/components/NewThisWeek/components/HoverPreview/components/HoverableDiv/HoverableDiv.js
+++ src/components/Home/components/NewThisWeek/components/HoverPreview/components/HoverableDiv/HoverableDiv.js
@@ -1,8 +1,9 @@
 const HoverableDiv = ({ handleMouseOver, handleMouseOut, image, title }) => {
+  const slicedTitle = title.length > 8 ? title.slice(0, 8) + '...' : title
   return (
     <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
       <img src={image} alt={title} className='new-this-week-picture' key={title}></img>
-      <div>{title}</div>
+      <div>{slicedTitle}</div>
     </div>
   );
 };
