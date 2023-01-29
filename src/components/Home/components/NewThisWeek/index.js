dedf4b3c0e40541cfe214c0bc0ebee6c54820d8a
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2023-01-29 22:12:03 +0800

    css

--- src/components/Home/components/NewThisWeek/index.js
+++ src/components/Home/components/NewThisWeek/index.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import useMedia from 'use-media';
 import Grid from '@mui/material/Grid';
 import HoverPreview from './components/HoverPreview/HoverPreview';
 import './index.css'
@@ -6,14 +8,18 @@ const NewThisWeek = (props) => {
   const { movies } = props
   const someMovies = movies.slice(0, 6)
 
+  const isSmall = useMedia('(max-width: 1300px)')
+  // const isNormal = useMedia('(min-width: 1300px)')
+  const md = isSmall ? 4 : 2;
+
   return (
     <div>
       <div className="new-this-week-section">New This Week</div>
-      <Grid container spacing={10} className='new-this-week-grid'>
+      <Grid container spacing={2} className='new-this-week-grid'>
         {someMovies.map((movie, index) => {
           const { backdrop_path, title, overview, vote_average } = movie
           return (
-            <Grid item xs={2} key={index}>
+            <Grid item xs={2} md={md} key={index}>
               <HoverPreview
                 image={backdrop_path}
                 title={title}
